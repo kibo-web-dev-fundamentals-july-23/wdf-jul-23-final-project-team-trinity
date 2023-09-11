75355f66c7a16551210d8b941a2c8f4a02f6f794
Cross-companion <mickdaniels101@gmail.com>
2023-09-11 19:24:12 +0100

    Finish

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 // Import a function from an external module.
 import { loadDescription } from "./description.js";
-import "./description.js";
+import "./teamView.js";
 
 // Select DOM elements using querySelector.
 const imgSlideshow = document.querySelector("#imgSlides");
@@ -55,45 +55,3 @@ function updateSwitchCircles(elementIndex) {
   const newActive = switchCirclesBtns[elementIndex];
   newActive.classList.add("switch-btn__circles--item-active");
 }
-
-// const imgSlideshow = document.querySelector("#imgSlides");
-const viewTeam = document.querySelector("#viewTeam");
-const viewTeamText = document.querySelector(".view-team__text");
-const viewTeamIcon = document.querySelector("#view-team__icon");
-const theTeam = document.querySelector("#theTeam");
-let isTeamView = false;
-
-function changeViewTeamText(open = true) {
-  viewTeamText.textContent = open ? "Back" : "Click!";
-  viewTeamIcon.style.rotate = open ? "180deg" : "0deg";
-  isTeamView = open ? true : false;
-}
-
-function openTeamView() {
-  if (!isTeamView) {
-    theTeam.scrollIntoView({ behavior: "smooth" });
-    changeViewTeamText();
-    isTeamView = true;
-  } else {
-    imgSlideshow.scrollIntoView({ behavior: "smooth" });
-    changeViewTeamText(false);
-    isTeamView = false;
-  }
-}
-
-viewTeam.addEventListener("click", (e) => {
-  openTeamView();
-});
-
-let options = {
-  rootMargin: "0px",
-  threshold: 0.3,
-};
-
-let observer = new IntersectionObserver((entries) => {
-  if (entries[0].isIntersecting !== true) return changeViewTeamText(false);
-
-  changeViewTeamText();
-}, options);
-
-observer.observe(theTeam);
